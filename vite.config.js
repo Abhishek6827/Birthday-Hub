21d9f42770faf9122a40005e20f9cca0ca39b80c
Abhishek <devilizbusy@gmail.com>
2025-10-14 23:07:08 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/Birthday-Hub/",
+  base: process.env.NODE_ENV === "production" ? "/Birthday-Hub/" : "/",
+  build: {
+    assetsDir: "assets",
+    cssCodeSplit: false, // Yeh line add karo
+  },
+  server: {
+    host: true,
+    port: 5173,
+  },
 });
